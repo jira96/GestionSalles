e210ab7f3abe788c8164edd625fd8252fa771bf3
jira96 <44611446+jira96@users.noreply.github.com>
2019-12-28 18:08:31 +0100

    Update SalleController.java

--- src/main/java/com/anji/controller/SalleController.java
+++ src/main/java/com/anji/controller/SalleController.java
@@ -1,5 +1,38 @@
 package com.anji.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.anji.entity.Salle;
+import com.anji.service.SalleService;
+
+@RestController
 public class SalleController {
 
+	@Autowired
+	private SalleService salleService;
+	
+	@GetMapping("/salles")
+	public List<Salle> getAllSalles() {
+		return salleService.getAllSalles();
+	}
+	
+	@GetMapping("/salles/findByCode/{code}")
+	public Salle getSalleByCode(@PathVariable("code") String code) {
+		return salleService.getSalleByCode(code);
+	}
+	
+	@PostMapping("/salles/add")
+	public Salle addOrUpdateSalle(@Valid @RequestBody Salle salle) {
+		return salleService.addOrUpdateSalle(salle);
+	}
+	
+	@DeleteMapping("/salles/delete/{Id}")
+	public void deleteSalle(@PathVariable("Id") Long id) {
+		salleService.deleteSalle(id);
+	}
 }
