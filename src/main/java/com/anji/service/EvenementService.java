0012d894035296c50aca7dd15cbd5d5b82d4968d
jira96 <44611446+jira96@users.noreply.github.com>
2019-12-28 18:15:59 +0100

    Update EvenementService.java

--- src/main/java/com/anji/service/EvenementService.java
+++ src/main/java/com/anji/service/EvenementService.java
@@ -1,5 +1,32 @@
 package com.anji.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anji.entity.Evenement;
+import com.anji.repository.EvenementRepository;
+
+@Service
 public class EvenementService {
 
+	@Autowired
+	private EvenementRepository evenementRepos;
+	
+	public List<Evenement> getAllEvents() {
+		return evenementRepos.findAll();
+	}
+	
+	public Evenement findByTitre(String titre) {
+		return evenementRepos.findByTitre(titre);
+	}
+	
+	public Evenement addOrUpdateEvenement(Evenement event) {
+		return evenementRepos.save(event);
+	}
+	
+	public void deleteEventById(Long id) {
+		evenementRepos.deleteById(id);
+	}
 }
