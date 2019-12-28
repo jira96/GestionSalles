47227f7fd7831b8ec99a567f2480c32d7aac7f01
jira96 <44611446+jira96@users.noreply.github.com>
2019-12-28 18:17:21 +0100

    Update SalleService.java

--- src/main/java/com/anji/service/SalleService.java
+++ src/main/java/com/anji/service/SalleService.java
@@ -1,5 +1,33 @@
 package com.anji.service;
 
-public class SalleService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anji.entity.Salle;
+import com.anji.repository.SalleRepository;
+
+@Service
+public class SalleService {
+	
+	@Autowired
+	private SalleRepository salleRepos;
+	
+	public List<Salle> getAllSalles() {
+		return salleRepos.findAll();
+	}
+	
+	public Salle getSalleByCode(String code) {
+		return salleRepos.findByCode(code);
+	}
+	
+	public Salle addOrUpdateSalle(Salle salle) {
+		return salleRepos.save(salle);
+	}
+	
+	public void deleteSalle(Long id) {
+		salleRepos.deleteById(id);
+	}
+	
 }
